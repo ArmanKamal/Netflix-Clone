fb8b8578bfae925b948df2ddb6453cf22cec0011
ArmanKamal <armankamal2421@gmail.com>
2021-02-24 16:04:43 -0600

    Added Header Component

---
+++ src/components/header/styles/header.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components/macro'
+import {Link} from  'react-router-dom'
+export const Background = styled.div`
+`;
+
+
+export const Frame = styled.div`
+`;
+
+export const Container = styled.div`
+`;
+
+export const Logo = styled.img`
+`;
+
+export const Button = styled(Link)`
+
+display: block;
+background: #e50914;
+width: 84px;
+
+`;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { FaqsContainer } from "../containers/faqs";
 import FooterContainer from "../containers/footer";
+import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from "../containers/jumbotron";
 function Home() {
     return (
         <>
-            <JumbotronContainer />
-            <FaqsContainer />
-            <FooterContainer />
+            <HeaderContainer>
+                <JumbotronContainer />
+                <FaqsContainer />
+                <FooterContainer />
+            </HeaderContainer>
         </>
     )
 }
