cc48aba673b9adf7421bc0f58ad6b920852b674e
ArmanKamal <armankamal2421@gmail.com>
2021-03-03 20:12:13 -0600

    Designed Register Page

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { Form } from "../components";
+import { useHistory } from "react-router-dom";
+import FooterContainer from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { FirebaseContext } from "../context/firebase";
+import * as ROUTES from "../constants/routes";
 
-function register() {
-    return (
-        <div>
-            Register Page
-        </div>
-    )
+export default function Register() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [firstName, setfirstName] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [error, seterror] = useState("");
+
+  const isInvalid = firstName === "" || password === "" || email === "";
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    //firebase
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignUp} method="POST">
+            <Form.Input
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setfirstName(e.target.value)}
+            />
+            <Form.Input
+              placeholder="Email Address"
+              value={email}
+              type="email"
+              onChange={(e) => setemail(e.target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              autoComplete="off"
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
+            <Form.Text>
+              Already a user? <Form.Link to="/login">Sign in now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot. Learn more.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
-
-export default register
