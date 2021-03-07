9c151cc329e54df95f55e2a2710013041c910e86
ArmanKamal <armankamal2421@gmail.com>
2021-03-06 19:40:30 -0600

    Added React Router auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import * as ROUTES from './constants/routes'
 import {Home, Browse, Login, Register} from "./pages/";
+import { IsUserRedirect} from './helpers/routes';
 
 const App = () => {
+  const user = {}
   return (
     <Router>
+       <Route exact path={ROUTES.LOGIN}>
+         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.LOGIN}>
+           <Login />
+         </IsUserRedirect>
+
+      </Route>
+
+      <Route exact path={ROUTES.SIGN_UP}>
+         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+           <Login />
+         </IsUserRedirect>
+
+      </Route>
+
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
